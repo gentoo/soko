7966a136fdb6833318752f36e1e6b6e99307aee1
Max Magorsch <arzano@gentoo.org>
2020-04-28 20:06:40 +0200

    Clean up the code of the changes atom feed
    Signed-off-by: Max Magorsch <arzano@gentoo.org>

--- pkg/app/handler/feeds/changes.go
+++ pkg/app/handler/feeds/changes.go
@@ -3,18 +3,16 @@ package feeds
 import (
 	"text/template"
 	"net/http"
-	"soko/pkg/logger"
 )
 
 // Show renders a template to show a given package
 func Changes(funcMap template.FuncMap, data interface{}, w http.ResponseWriter) {
-	logger.Info.Println("Changes atom template")
 	templates := template.Must(
 				template.New("changes.atom").
 					Funcs(funcMap).
 					ParseGlob("web/templates/feeds/changes.atom.tmpl"))
 
-	//w.Header().Set("Content-Type", "application/atom+xml")
+	w.Header().Set("Content-Type", "application/atom+xml")
 	templates.ExecuteTemplate(w, "changes.atom.tmpl", data)
 }
 
