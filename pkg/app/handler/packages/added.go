1ea72450e1ba1c178959426ece6cee0b1b2f7b6a
Alec Warner <antarus@gentoo.org>
2020-05-26 00:38:53 -0700

    Rewrite added feed with gorilla feed.
    Signed-off-by: Alec Warner <antarus@gentoo.org>

--- pkg/app/handler/packages/added.go
+++ pkg/app/handler/packages/added.go
@@ -3,18 +3,43 @@
 package packages
 
 import (
+	"fmt"
+	"time"
 	"net/http"
-	"soko/pkg/app/handler/feeds"
+	"github.com/gorilla/feeds"
 )
 
-// Added renders a template containing
-// a list of 50 recently added versions
+// Added renders a template containing a list of 50 recently added versions.
 func Added(w http.ResponseWriter, r *http.Request) {
 	addedVersions := getAddedVersions(50)
 	RenderPackageTemplates("changedVersions", "changedVersions", "changedVersionRow", GetFuncMap(), CreateFeedData("Added", addedVersions), w)
 }
 
 func AddedFeed(w http.ResponseWriter, r *http.Request) {
-	addedVersions := getAddedVersions(1000)
-	feeds.Changes(GetTextFuncMap(), CreateFeedData("Added", addedVersions), w)
+	addedVersions := getAddedVersions(10)
+	feed := &feeds.Feed{
+		Title: "Added packages in Gentoo.",
+		Description: "Added packages in Gentoo.",
+		Author: &feeds.Author{Name: "Gentoo Packages Database"},
+		Created: time.Now(),
+		Link: &feeds.Link{Href: "https://packages.gentoo.org"},
+	}
+	for _, added := range addedVersions {
+		item := &feeds.Item{
+			Title: added.Atom,
+			Link: &feeds.Link{Href: fmt.Sprintf("https://packages.gentoo.org/package/%s", added.Atom)},
+			Description: added.Description,
+			Author: &feeds.Author{Name: "Unknown"},
+			Created: time.Now(),
+		}
+		if len(added.Commits) > 0 {
+			lastCommit := added.Commits[0]
+			item.Author = &feeds.Author{Name: lastCommit.CommitterName}
+			item.Created = lastCommit.CommitterDate
+			item.Content = fmt.Sprintf("%s is now available in Gentoo on these architectures: %s. See Commit: https://gitweb.gentoo.org/repo/gentoo.git/commit/?id=%s",
+				added.Atom, added.Keywords, lastCommit.Id)
+		}
+		feed.Add(item)
+	}
+	feed.WriteAtom(w)
 }
