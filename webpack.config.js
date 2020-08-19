df45fbae1c1dd12afac6617df3a4d9648f108fd4
Max Magorsch <arzano@gentoo.org>
2020-08-20 01:46:33 +0200

    Add png files as well
    Signed-off-by: Max Magorsch <arzano@gentoo.org>

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
                 ],
             },
             {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                test: /\.(woff(2)?|ttf|eot|svg|png)(\?v=\d+\.\d+\.\d+)?$/,
                 use: [
                     {
                         loader: 'file-loader',
