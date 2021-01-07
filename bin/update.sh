2a16fcdcdf721bcf21d47850ad0d6fc5507490ed
Robin H. Johnson <robbat2@gentoo.org>
2021-01-06 21:36:50 -0800

    bin: make updates more reliable and faster
    `git pull --rebase` has many corner cases where it tries to do a "safe"
    update and may leave the working tree in a unsuitable state for
    automated processes.
    
    Since the working git repo is meant to be entirely automated, split it
    out to a git-fetch of just the branch we need, and then forcibly reset
    the tree content to the tip of that branch after each fetch.
    
    Signed-off-by: Robin H. Johnson <robbat2@gentoo.org>

--- bin/update.sh
+++ bin/update.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+: "${GIT_URI:=https://anongit.gentoo.org/git/repo/gentoo.git}"
+: "${GIT_BRANCH:=master}"
+: "${GIT_REMOTE:=origin}"
+
 update_repository(){
   # This is the copy of the tree used to run gpackages against.
   if [[ ! -d /mnt/packages-tree/gentoo/ ]]; then
       cd /mnt/packages-tree || exit 1
-      git clone https://anongit.gentoo.org/git/repo/gentoo.git
+      git clone \
+        --quiet \
+        --single-branch \
+        --branch "${GIT_BRANCH}" \
+        --origin "${GIT_REMOTE}" \
+        "${GIT_URI}"
   else
       cd /mnt/packages-tree/gentoo/ || exit 1
-      git pull origin master --rebase &>/dev/null
+      if [ "$(git remote get-url "${GIT_REMOTE}")" != "${GIT_URI}" ]; then
+          git remote set-url "${GIT_REMOTE}" "${GIT_URI}"
+      fi
+      git fetch --quiet --force "${GIT_REMOTE}" "${GIT_BRANCH}"
+      git reset --quiet --hard
   fi
 }
 
