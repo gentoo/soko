56de240b7a6b44025bd48ae8ee70a08681f1eaf4
Arthur Zamarin <arthurzam@gentoo.org>
2024-09-07 09:21:09 +0300

    drop ia64, replace in tables with mips
    Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

--- pkg/models/userpreferences.go
+++ pkg/models/userpreferences.go
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 package models
 
-var ArchesToShow = [...]string{"amd64", "x86", "alpha", "arm", "arm64", "hppa", "ia64", "ppc", "ppc64", "riscv", "sparc"}
+var ArchesToShow = [...]string{"amd64", "x86", "alpha", "arm", "arm64", "hppa", "mips", "ppc", "ppc64", "riscv", "sparc"}
 var AllArches = [...]string{"alpha", "amd64", "arm", "arm64", "hppa", "ia64", "mips", "ppc", "ppc64", "riscv", "s390", "sparc", "x86"}
 var OldCookieNames = [...]string{"search_history", "userpref_general", "userpref_packages", "userpref_maintainers", "userpref_useflags", "userpref_arches"}
