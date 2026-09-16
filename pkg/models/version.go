b6e51779670aa5a425e24aa2a8f02e7114633e35
Arthur Zamarin <arthurzam@gentoo.org>
2026-09-16 23:05:37 +0300

    version: drop leftover debug output
    Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

--- pkg/models/version.go
+++ pkg/models/version.go
@@ -5,7 +5,6 @@
 package models
 
 import (
-	"fmt"
 	"math/big"
 	"regexp"
 	"sort"
@@ -70,13 +69,6 @@ func (v Version) BuildDepMap() []versionDepMap {
 func (v *Version) GreaterThan(other Version) bool {
 	versionIdentifierA := v.computeVersionIdentifier()
 	versionIdentifierB := other.computeVersionIdentifier()
-
-	if v.Version == "0.7.8_p0-r100" {
-		fmt.Println("versionIdentifierA")
-		fmt.Println(versionIdentifierA)
-		fmt.Println("versionIdentifierB")
-		fmt.Println(versionIdentifierB)
-	}
 
 	// compare the numeric part
 	numericPartsA := strings.Split(versionIdentifierA.NumericPart, ".")
