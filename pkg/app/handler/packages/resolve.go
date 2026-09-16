bec54b4e22e488b8cfd43eb838fe2f69bae4f0e2
Arthur Zamarin <arthurzam@gentoo.org>
2026-09-16 23:04:08 +0300

    /packages/resolve.json: return 404 instead of panicking on no match
    Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

--- pkg/app/handler/packages/resolve.go
+++ pkg/app/handler/packages/resolve.go
@@ -26,9 +26,15 @@ func Resolve(w http.ResponseWriter, r *http.Request) {
 		Limit(1).
 		Select()
 
-	if err != nil && err != pg.ErrNoRows {
+	if err == pg.ErrNoRows {
+		http.NotFound(w, r)
+		return
+	} else if err != nil {
 		http.Error(w, http.StatusText(http.StatusInternalServerError),
 			http.StatusInternalServerError)
+		return
+	} else if len(gpackage.Versions) == 0 || len(gpackage.Commits) == 0 {
+		http.NotFound(w, r)
 		return
 	}
 
